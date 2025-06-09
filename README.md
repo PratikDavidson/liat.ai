cb64b95e6a4e1dba9cc017e01f575afd36e87718
PratikDavidson <www.david.pratik@gmail.com>
2025-06-10 00:09:44 +0530

    Added initial base code

---
+++ README.md
@@ -0,0 +1,36 @@
+# Player Re-Identification in Sports Footage
+
+This repository provides a solution to one of the task assigned to assess the ability to solve real-world computer vision problem in sport analytics. 
+
+## Task: Re-Identification in a Single Feed
+
+Identification of each player ensuring same player retains the same ID even after going out of view in a given 15-second video.
+
+### Getting Started
+
+To get a local copy up and running, follow these simple steps.
+
+### Prerequisites
+
+Install [**Miniconda**](https://www.anaconda.com/docs/getting-started/miniconda/install)
+
+### Installation & Setup
+
+_Below are steps to follow in setting up the project._
+
+1. Create a new conda environment
+```
+conda create --name pytorch python=3.10
+```
+2. Activate the environment
+```
+conda activate pytorch
+```
+3. Install dependencies
+```
+pip install ultralytics
+```
+4. Run
+```
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,38 @@
+# Import required packages
+import cv2
+from ultralytics import YOLO
+
+# Load the Ultalytics YOLO11 model
+model = YOLO("models/best.pt")
+
+# Open the video file
+video_path = "data/15sec_input_720p.mp4"
+cap = cv2.VideoCapture(video_path)
+
+# Loop through the video frames
+while cap.isOpened():
+    # Read individual frames from the video
+    success, frame = cap.read()
+
+    if success:
+        # Run YOLO11 tracking on the frame, persisting tracks between frames
+        results = model.track(frame, persist=True)
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Display the annotated frame
+        cv2.imshow("YOLO11 Tracking", annotated_frame)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        # Break the loop if the end of the video is reached
+        break
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
+
+#model.predict("in/test.png", save=True, conf=0.5)
